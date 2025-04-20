45b727e4ccb6888eb1718a5c8ab66bf05f4b497b
dap3884 <dap3@iu.edu>
2025-04-20 17:43:27 -0400

    Completed backend

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/merge")
+@RequestMapping("/merger")
 public class MergerController {
 
     MergerRepository mergerRepository;
